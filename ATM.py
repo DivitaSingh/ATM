d64a6dbbe468580d27875744b0071e7f14ad29e8
DivitaSingh <75487683+DivitaSingh@users.noreply.github.com>
2022-07-12 19:58:40 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,13 @@
+class ATM(object):
+     def __init__(self,ATMCard, PinNumber):
+        self.ATMCard=ATMCard
+        self.PinNumber=PinNumber
+
+     def CashWidrawl():
+        print ("Cash Widrawed")
+
+     def BalanceEnquiry():
+        print("Balance = ₹ 10,000 ")
+
+     def CashTransfer():
+        print("Cash has been Transfered")
